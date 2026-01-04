69b5f6362b65a2429fb80743a2898c1a0fbb7f9c
Sijal Manandhar <sijal.m06@gmail.com>
2026-01-04 10:22:27 +0545

    add active state helpers, calculation logic, and error handling

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,45 @@
+const tipAmountEle = document.getElementById("tip-amount");
+const totalEle = document.getElementById("total");
 const resetButton = document.getElementById("reset-button");
+const tipOptions = document.getElementsByClassName("tip-option");
+
+const errorElements = document.getElementsByClassName("error");
+const errorDict = Array.from(errorElements).reduce((acc, el) => {
+  const key = el.id.split("-")[0];
+  acc[key] = el;
+  return acc;
+}, {});
 
 let values = {};
-const requiredInputIds = ["bill", "tip", "number-of-people"];
+let errors = {};
+const requiredInputIds = ["bill", "tip", "people"];
+
+function isValuesSet(values) {
+  return requiredInputIds.every(
+    (item) => values.hasOwnProperty(item) && values.item !== 0,
+  );
+}
 
 const inputFields = Array.from(document.getElementsByTagName("input")).filter(
   (ele) => requiredInputIds.includes(ele.getAttribute("id")),
 );
 
-Array.from(document.getElementsByClassName("tip-option")).forEach((ele) => {
-  ele.addEventListener("click", (e) => {
-    const activeTipOptions = [...document.getElementsByClassName("active")];
-    if (activeTipOptions.length > 0) {
+function removeActiveClassInTipOption(ele) {
+  const activeTipOptions = [...document.getElementsByClassName("active")];
+  if (activeTipOptions.length > 0) {
+    if (ele) {
       const isOptionDoubleClick = activeTipOptions.includes(ele);
-      if (isOptionDoubleClick) {
-        return;
-      }
-      activeTipOptions.forEach((ele) => ele.classList.remove("active"));
+      if (isOptionDoubleClick) return;
     }
+    activeTipOptions.forEach((ele) => ele.classList.remove("active"));
+  }
+}
+
+Array.from(tipOptions).forEach((ele) => {
+  ele.addEventListener("click", (e) => {
+    removeActiveClassInTipOption(ele);
     const value = ele.innerHTML.trim().replace("%", "");
-    handleInputChange("number-of-people", value);
+    handleInputChange("tip", value);
     ele.classList.add("active");
   });
 });
@@ -26,6 +47,7 @@ Array.from(document.getElementsByClassName("tip-option")).forEach((ele) => {
 resetButton.addEventListener("click", (e) => {
   e.target.setAttribute("disabled", true);
   inputFields.forEach((ele) => (ele.value = ""));
+  removeActiveClassInTipOption();
   values = {};
 });
 
@@ -35,18 +57,53 @@ function isValidNumber(value) {
   return strValue !== "" && !isNaN(strValue) && isFinite(strValue);
 }
 
+function calculateResults(bill, tipPercent, people) {
+  if (![bill, tipPercent, people].every((val) => Boolean(val)) || people <= 0) {
+    console.log("empty");
+    return { tipAmount: 0, total: 0 };
+  }
+  const totalTip = bill * (tipPercent / 100);
+  const tipPerPerson = totalTip / people;
+  const totalPerPerson = (bill + totalTip) / people;
+  return {
+    tipAmount: tipPerPerson.toFixed(2),
+    total: totalPerPerson.toFixed(2),
+  };
+}
+
+function checkErrors(errors) {
+  const errorKeys = Object.keys(errors);
+  if (errorKeys.length > 0) {
+    errorKeys.forEach((error) => (errorDict[error].innerHTML = errors[error]));
+  } else {
+    Array.from(errorElements).forEach((ele) => (ele.innerHTML = ""));
+  }
+}
+
 function toggleResetButton() {
-  const isInputFieldsEmpty = !inputFields.every((ele) => Boolean(ele.value));
-  if (isInputFieldsEmpty) resetButton.setAttribute("disabled", true);
+  if (!isValuesSet(values)) resetButton.setAttribute("disabled", true);
   else resetButton.removeAttribute("disabled");
 }
 
 function handleInputChange(name, value) {
+  errors = {};
   if (!requiredInputIds.includes(name)) return;
+  if (name === "tip") removeActiveClassInTipOption();
 
-  if (!isValidNumber(value)) values[name] = 0;
-  else values[name] = Number(value);
+  const numValue = Number(value);
+  const isInvalid = !isValidNumber(value);
+  values[name] = isInvalid || numValue === 0 ? 0 : numValue;
+  if (isInvalid) errors[name] = "Not valid input.";
+  else if (numValue === 0) errors[name] = "Can't be zero.";
 
+  if (isValuesSet(values)) {
+    const { bill, tip, people } = values;
+    const { tipAmount, total } = calculateResults(bill, tip, people);
+    tipAmountEle.innerHTML = `$${tipAmount}`;
+    totalEle.innerHTML = `$${total}`;
+  }
+
+  checkErrors(errors);
   toggleResetButton();
 }
 
